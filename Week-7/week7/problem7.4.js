7b3fc63ec2e5ed95acd052dbae42b6fc57506775
santy <saisantosh8500@gmail.com>
2022-11-18 15:55:38 +0530

    week7

---
+++ Week-7/week7/problem7.4.js
@@ -0,0 +1,44 @@
+function areBracketsBalanced(expr)
+{
+    let stack = [];
+    for(let i = 0; i < expr.length; i++)
+    {
+        let x = expr[i];
+ 
+        if (x == '(' || x == '[' || x == '{')
+        {
+             
+            stack.push(x);
+            continue;
+        }
+        if (stack.length == 0)
+            return false;
+             
+        let check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+ 
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+ 
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+    return (stack.length == 0);
+}
+let expr = "([{}])";
+if (areBracketsBalanced(expr))
+    console.log("Balanced ");
+else
+    console.log("Not Balanced ");
