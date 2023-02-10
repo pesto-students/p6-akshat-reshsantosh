476deabfa2900d4d425950a8a0d2a8812ee3283b
unknown <saisantosh8500@gmail.com>
2023-02-10 16:11:43 +0530

    week-16

---
+++ Week-16/anime_app/src/Components/AddToList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+// import { ReactGA } from 'react-ga';
+// import { useContext } from 'react';
+// import { UseEffect } from "@sentry/react/types/types";
+// import { useEffect } from 'react';
+
+
+export const AddToList = () => {
+//  useEffect(()=>{
+//   ReactGA.pageview(window.location.pathname)
+//  })
+
+  return (
+    <>
+        <div className="mylist">
+            <p>Add To List +</p>
+        </div>
+    </>
+  )
+}
